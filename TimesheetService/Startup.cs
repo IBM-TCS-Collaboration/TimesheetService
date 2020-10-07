5d021b17f1ce8a24a8510e88d5a52ec41e01ec91
taskeen16 <65477432+taskeen16@users.noreply.github.com>
2020-10-07 16:21:24 +0530

    CORS issue fixed

--- TimesheetService/Startup.cs
+++ TimesheetService/Startup.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -25,6 +25,10 @@ namespace TimesheetService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin());
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v1", new Info
@@ -45,6 +49,7 @@ namespace TimesheetService
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors(options => options.AllowAnyOrigin());
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
